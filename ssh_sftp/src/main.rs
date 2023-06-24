ad929c9d181dcca171fb43ed6c26587b9f88efb8
spindly <kaihrmt@gmail.com>
2023-06-24 04:37:20 +0100

    creating esblishing tcp and sftp

--- ssh_sftp/src/main.rs
+++ ssh_sftp/src/main.rs
@@ -1,50 +1,73 @@
 use ssh2::{Sftp, Session};
-use std::{io::{Read, self}, collections::linked_list, result};
-fn main() {
-    println!("Hello, world!");
+use std::{io::{Read, self},net::{ TcpStream}};
+
+struct sftp{
+    hostname:String,
+    port:String,
+    host_port:String,
+    username:String,
+    password:String, //storing password here maybe a potentually bad idea
+    session: Session,
+    alive:bool,
+    server_selected:bool,
 }
 
-fn stfp_initate_connect(hostname:String,port:String,username:String,password:String){
+fn sftp_build(hostname:String,port:String,
+    username:String,password:String)->sftp{
+        let host_port:String = format!("{}:{}",&hostname.clone(),&port.clone());
+        let tcp:TcpStream = std::net::TcpStream::connect(&host_port).unwrap();
+        let mut session = Session::new().unwrap();
+        session.set_tcp_stream(tcp);
+        session.handshake().unwrap();
+        session.userauth_password(&username.clone(), &password.clone()).unwrap();
+        assert!(session.authenticated());
+        let mut alive:bool = true;
+        let mut server_selected:bool = true;
+    
+        let sftp = session.sftp().unwrap();
 
-    let host_port:String = format!("{}:{}",&hostname,&port);
-    let tcp = std::net::TcpStream::connect(host_port).unwrap();
-    let mut session = Session::new().unwrap();
-    session.set_tcp_stream(tcp);
-    session.handshake().unwrap();
-    session.userauth_password(&username, &password).unwrap();
-    assert!(session.authenticated());
+        let sftp_client:sftp = sftp { hostname: hostname, port: port
+            , host_port: host_port, username: username
+            , password: password,session:session,
+            alive:alive,server_selected:server_selected};
 
-    let mut sftp = session.sftp().unwrap();
-
+        return sftp_client;
 }
 
-fn sftp_main (sftp_session:&Session,ssh2_session:&Session){
-    let mut alive:bool = true;
-    let mut server_selected:bool = true;
-    let mut input = String::new();
-    while alive{
+fn sftp_main (sftp_client:&mut sftp){
+    let mut input:String = String::new();
+    while sftp_client.alive{
         std::io::stdin().read_line(&mut input).expect("failed to read input");
-        sftp_choice(userinput, sftp_session, ssh2_session);
 
+        sftp_choice(&input,sftp_client);
     }
 
 }
 
-fn sftp_choice(userinput:String, sftp_session:Option<&Session>,
-    ssh2_session:Option<&Session>)-> i8
+fn sftp_choice(userinput:&String, sftp_client:&mut sftp)-> i8
     {
     let mut return_value:i8 = 0;
     // if it returns a 0 then its correct
     //anything else is either a error condition or a condition
     if userinput == "exit"{
-        return_value = 1;
+        sftp_client.alive = false;
         return return_value;
     }
-    else if userinput == "ls"{
+    else if userinput == "ls"{  
+        return return_value;
+    }
+    else if userinput == "sw"{
+        let invert:bool =sftp_client.server_selected;
+        sftp_client.server_selected = !invert;
         return return_value;
     }
     else{
         return  return_value;
     }
 
+}
+
+
+fn main() {
+    println!("Hello, world!");
 }
